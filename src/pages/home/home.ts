8648325425b45f8315661d68668d2dd149ca646b
yusefrich <yusef.rick@gmail.com>
2018-11-24 16:23:12 -0300

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,33 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public peso:string;
+  public altura:string;
+  public calculo:string;
+  public valorImc:number;
+  public classificacao:string;
 
-  constructor(public navCtrl: NavController) {
+  calculoIMC(){
+    let pesoValue = parseFloat(this.peso);
+    let alturaValue = parseFloat(this.altura);
+    this.valorImc = pesoValue / (alturaValue * alturaValue);
+
+    if(this.valorImc < 18.5){
+      this.classificacao = "Abaixo do peso";
+    }else if(this.valorImc >= 18.5 && this.valorImc < 24.9){
+      this.classificacao = "Peso normal";
+    }else if(this.valorImc >= 24.9 && this.valorImc <= 29.9){
+      this.classificacao = "Sobrepeso";
+    }else if(this.valorImc > 29.9 && this.valorImc <= 34.9){
+      this.classificacao = "Obesidade grau 1";
+    }else if(this.valorImc > 34.9 && this.valorImc <= 39.9){
+      this.classificacao = "Obesidade grau 2";
+    }else if(this.valorImc >= 40){
+      this.classificacao = "Obesidade grau 3 ou mórbida";
+    }
 
   }
 
+  constructor(public navCtrl: NavController) {
+  }
 }
